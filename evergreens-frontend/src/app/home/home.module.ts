6a116982d66d7f007cac2b3f0a9b85cb6861a1a2
Onesmus Wambugu <onesmus1024@gmail.com>
2023-02-26 03:22:37 +0300

    added home page module

---
+++ evergreens-frontend/src/app/home/home.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero/hero.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { EnticementComponent } from './enticement/enticement.component';
+import { PopularPicksComponent } from './popular-picks/popular-picks.component';
+import { SinglePopularComponent } from './single-popular/single-popular.component';
+
+
+const routes :Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  }
+]
+
+
+
+@NgModule({
+  declarations: [
+    HeroComponent,
+    HomePageComponent,
+    CategoryComponent,
+    EnticementComponent,
+    PopularPicksComponent,
+    SinglePopularComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class HomeModule { }
